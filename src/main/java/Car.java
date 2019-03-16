4c326a177b9bb29b4d4121d14f7bf4646c19e5b3
habibzuhaer <123pxdfg7Pxx>
2019-03-16 17:20:44 +0100

    car_two_text

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,2 +1,64 @@
 public class Car {
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getDrzwi() {
+        return drzwi;
+    }
+
+    public void setDrzwi(int drzwi) {
+        this.drzwi = drzwi;
+    }
+
+    public int getMarka() {
+        return marka;
+    }
+
+    public void setMarka(int marka) {
+        this.marka = marka;
+    }
+
+    public int getRok() {
+        return rok;
+    }
+
+    public void setRok(int rok) {
+        this.rok = rok;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "color='" + color + '\'' +
+                ", price=" + price +
+                ", drzwi=" + drzwi +
+                ", marka=" + marka +
+                ", rok=" + rok +
+                '}';
+    }
+
+    private String color;
+
+    private int price;
+
+    private int drzwi;
+
+    private int marka;
+
+    private int rok;
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,39 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    public static void main (String[]args) {
+        Scanner k = new Scanner(System.in);
+
+        System.out.println("Color ?");
+        String color = k.nextLine();
+
+
+        System.out.println("Price?");
+        int price = k.nextInt();
+
+        System.out.println("Drzwi?");
+        int drzwi = k.nextInt();
+
+
+        System.out.println("Marka?");
+        String marka = k.next();
+
+
+        System.out.println("Rok ?");
+        int rok = k.nextInt();
+
+       // ArrayList.class
+
+        Car car = new Car();
+        car.getColor();
+        car.getDrzwi();
+        car.getMarka();
+        car.getPrice();
+        car.getRok();
+
+
+
+    }
+
 }
